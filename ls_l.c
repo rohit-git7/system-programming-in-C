c5a1306ab3169bd0eecc7ffe7a7c3f1bd3f41d92
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-03 03:01:55 +0530

    Update ls_l.c
    Implementing linux command 'ls -l directory_name' in C
    Compile and run the executable:
         ./exectuable path_to_directory

--- ls_l.c
+++ ls_l.c
@@ -1,21 +1,26 @@
+/*
+Implementing linux command 'ls -l directory_name' in C i.e a detailed view of file permissions, number of links,
+username, groupname, file size, time of modification, file name.
+*/
 #include<stdio.h>
-#include<dirent.h>
+#include<dirent.h> //struct dirent,opendir()
 #include<string.h>
-#include<sys/stat.h>
-#include<grp.h>
-#include<pwd.h>
-#include<time.h>
+#include<sys/stat.h> // stat()
+#include<grp.h> //getgrgid(),struct group
+#include<pwd.h> //getpwuid(),struct passwd
+#include<time.h> //strftime(), struct tm
 
 int main(int argc,char *argv[])
 {
  	char dir_name[100];
 	char time_buff[200]; 
 	struct dirent *entry;
-	struct stat buff;
-	struct tm *info;
-	struct group *gp;		
-	struct passwd *pw;
+	struct stat buff; //to store complete information about file
+	struct tm *info; // to resolve st_mtime i.e time_t format to string format
+	struct group *gp; // structure that gives groupname for group id
+	struct passwd *pw; // struture that gives username for user id
 
+// run program for current directory if no argument is passed
 	 if(argc == 1)
 	{
 		strcpy(dir_name,".");
@@ -25,77 +30,83 @@ int main(int argc,char *argv[])
 		strcpy(dir_name,argv[1]);
 	}
 	
-	DIR *d = opendir(dir_name);
+	DIR *d = opendir(dir_name);//open directory list
 
-	while((entry = readdir(d))!=NULL)
+	while((entry = readdir(d))!=NULL)// read each file one by one
 	{
-		stat(entry->d_name,&buff);
-	   	struct group *gp = getgrgid(buff.st_gid);		
-		struct passwd *pw = getpwuid(buff.st_uid);
-		info = localtime(&(buff.st_mtime));
-		strftime(time_buff,sizeof(time_buff),"%b %d %H:%M",info);
-	   if(strcmp(entry->d_name,".")!=0 && strcmp(entry->d_name,"..")!=0)
+		stat(entry->d_name,&buff); // get complete file information in object of structure stat
+	   	struct group *gp = getgrgid(buff.st_gid);// get groupname from groupid
+		struct passwd *pw = getpwuid(buff.st_uid);//get username from user id
+		info = localtime(&(buff.st_mtime)); // getting mtime details in structure tm
+		strftime(time_buff,sizeof(time_buff),"%b %d %H:%M",info); //getting required details from structure to string time_buff
+	   if(strcmp(entry->d_name,".")!=0 && strcmp(entry->d_name,"..")!=0 && entry->d_name[0]!='.')//skip hidden files
  		{
- 
-		if(buff.st_mode & S_IFREG)
+ // checking file type
+		if(buff.st_mode & S_IFREG) //file is regullar file
 			printf("-");
 		else
-		 if(buff.st_mode & S_IFDIR)
+		 if(buff.st_mode & S_IFDIR)// file is directory
                         printf("d");
 		else
-		  if(buff.st_mode & S_IFBLK)
+		  if(buff.st_mode & S_IFBLK)// file is block device
 			printf("b");
 		else
-		  if(buff.st_mode & S_IFCHR)
+		  if(buff.st_mode & S_IFCHR)// file is character device
 			printf("c");
-
-		if(buff.st_mode & S_IRUSR)
+//checking permission bits that are set for file
+		if(buff.st_mode & S_IRUSR)//owner can read
 			printf("r");
 		else
 			printf("-");
 
-		if(buff.st_mode & S_IWUSR)
+		if(buff.st_mode & S_IWUSR)//owner can write
 			printf("w");
 		else
 			printf("-");
 
-		if(buff.st_mode & S_IXUSR)
+		if(buff.st_mode & S_IXUSR)// owner can execute
 			printf("x");
 		else
 			printf("-");
 
-		if(buff.st_mode & S_IRGRP)
+		if(buff.st_mode & S_IRGRP)// members of group can read
 			printf("r");
 		else
 			printf("-");
 
-		if(buff.st_mode & S_IWGRP)
+		if(buff.st_mode & S_IWGRP)//members of group can write
 			printf("w");
 		else
 			printf("-");
 
-		if(buff.st_mode & S_IXGRP)
+		if(buff.st_mode & S_IXGRP)// members of group can execute
 			printf("x");
 		else
 			printf("-");
 
-		if(buff.st_mode & S_IROTH)
+		if(buff.st_mode & S_IROTH)// everyone can read
 			printf("r");
 		else
 			printf("-");
 
-		if(buff.st_mode & S_IWOTH)
+		if(buff.st_mode & S_IWOTH)//everyone can write
 			printf("w");
 		else
 			printf("-");
 			
-		if(buff.st_mode & S_IXOTH)
+		if(buff.st_mode & S_IXOTH)//everyone can execute
 			printf("x");
 		else
 			printf("-");
 		
 		printf(" %d %s %s %4d %s %s\n",buff.st_nlink,pw->pw_name,gp->gr_name,(int)buff.st_size,time_buff,entry->d_name);
 		}
+	/*
+	st_nlink= number of links
+	pw_name = owner name
+	gr_name= group name
+	st_size = size of file
+	*/
 	}
 	return 0;
 }
