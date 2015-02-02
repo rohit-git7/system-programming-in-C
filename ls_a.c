044e12292adf4164d4e5411841ba9259a8dec5b5
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-03 02:44:31 +0530

    Update ls_a.c
    Implementing linux command 'ls -a directory_name'
    Compile and run the executable:
         ./exectuable directory_name

--- ls_a.c
+++ ls_a.c
@@ -1,3 +1,6 @@
+/*
+Implementing linux command 'ls -a dir_name' in C i.e to list all files(including hidden files)
+*/
 #include<stdio.h>
 #include<dirent.h>
 #include<string.h>
@@ -5,7 +8,7 @@
 int main(int argc,char *argv[])
 {
  	char dir_name[100];
- 
+ //if no directory name is given by user, run for current directory
 	 if(argc == 1)
 	{
 		strcpy(dir_name,".");
@@ -15,11 +18,11 @@ int main(int argc,char *argv[])
 		strcpy(dir_name,argv[1]);
 	}
 
-	DIR *d = opendir(dir_name);
+	DIR *d = opendir(dir_name);//open directory list
 	struct dirent *entry;
-	while((entry = readdir(d))!=NULL)
+	while((entry = readdir(d))!=NULL)//reading directory
 	{
- 			printf("%s\n",entry->d_name);
+ 		printf("%s\n",entry->d_name);
 	}
 	return 0;
 }
