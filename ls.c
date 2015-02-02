b88cbe67104103b4d4b7f8b0148047f6bde7dcc8
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-03 02:45:58 +0530

    Update ls.c
    Implementing linux command 'ls directory_name' in C
    Compile and run the executable:
         ./exectuable path_to_directory

--- ls.c
+++ ls.c
@@ -20,7 +20,7 @@ int main(int argc,char *argv[])
 
 	DIR *d = opendir(dir_name); // to open directory
 	struct dirent *entry;
-	while((entry = readdir(d))!=NULL) //read all entries
+	while((entry = readdir(d))!=NULL) //read all entries one by one
 	{
 	    if(strcmp(entry->d_name,".")!=0 && strcmp(entry->d_name,"..")!=0 && entry.d_name[0]!='.')//don't print names of hidden files
  			printf("%s\n",entry->d_name);
