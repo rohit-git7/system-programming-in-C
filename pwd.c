f7651afd83fd81fe1e1f340e19b7b59f2885d0f0
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-04 04:12:02 +0530

    Update pwd.c

--- pwd.c
+++ pwd.c
@@ -1,10 +1,13 @@
+/*
+Implementing linux command 'pwd' in C
+*/
+
 #include<stdio.h>
-#include<string.h>
 
 int main(int argc,char *argv[])
 {
 	char dir_name[1024];
-	getcwd(dir_name,sizeof(dir_name));
+	getcwd(dir_name,sizeof(dir_name));//get name of current working directory in string
 	printf("%s\n",dir_name);
 	
 return 0;
