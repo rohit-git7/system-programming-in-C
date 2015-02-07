1870e832328b330b12e0c86eb5f72464a5e8a538
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-07 22:37:44 +0530

    Update fork.c

--- fork.c
+++ fork.c
@@ -1,21 +1,29 @@
+/*
+Program to create and terminate processes (using fork(), wait(), exit())
+getpid(): Get process ID
+getppid(): get parent process ID
+*/
 #include<stdio.h>
-#include<unistd.h>
+#include<stdlib.h>//exit()
+#include<unistd.h>//pid_t
 int main()
 {
 	int status;
 	pid_t pd;
 	pd = fork();
-	if(pd == -1)
+	if(pd == -1)//fork() failed
 	{
 		perror("Error");
+		exit(0);
 	}
-	else if(pd == 0)
+	else if(pd == 0)//child process
 	{
 		printf("Child process with process ID %d with parent process ID %d\n",getpid(),getppid());
 	}
-	else
+	else// parent process
 	{
-		wait(&status);
+		wait(&status);// wait for the completion of child process
+		// It is important to prevent creation of zombie processes
 		printf("Parent process %d\n",getpid());
 	}
 
