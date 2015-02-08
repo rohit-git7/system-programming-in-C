614d95b2ced5ada6a00281bdf5ae840d83dc0acc
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-08 23:13:10 +0530

    Update cd.c

--- cd.c
+++ cd.c
@@ -1,9 +1,14 @@
+/*
+Implement linux command cd.
+To run:
+	./executable directory-name
+*/
 #include<stdio.h>
 int main(int argc, char *argv[])
 {
 	char buffer[1024];
 	/*
-	if(chdir("..")!=-1)
+	if(chdir("..")!=-1)// to show usage of chdir()
 	{
 		getcwd(buffer,sizeof(buffer));
         	printf("%s\n",buffer);
@@ -14,10 +19,10 @@ int main(int argc, char *argv[])
 	}*/
 	if(chdir(argv[1])!=-1)
 	{
-		getcwd(buffer,sizeof(buffer));
+		getcwd(buffer,sizeof(buffer));//to get name of current working directory
         	printf("Directory changed to %s\n",buffer);
 	}
-	else
+	else//returns -1 in case of an error
 	{
 		perror("Error");
 	}
