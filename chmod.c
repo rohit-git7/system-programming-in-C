e544eebc5103509b78f47e87afc9367c2544d3e7
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-13 23:57:39 +0530

    Update chmod.c

--- chmod.c
+++ chmod.c
@@ -1,15 +1,29 @@
+/*
+Implementing linux command 'chmod' in C.
+To run:
+	./executable mode filename
+*/
 #include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<string.h>
+#include<stdlib.h>//exit()
+#include<sys/types.h>//mode_t
+#include<string.h>//strtol()
 int main(int argc, char *argv[])
 {
+	if(argc < 3)
+	{
+		printf("Too few arguments\n");
+		exit(0);
+	}
+	else if(argc > 3)
+	{
+		printf("Too many arguments\n");
+		exit(0);
+	}
+	
 	int res;
-	mode_t mode = strtol(argv[1],NULL,8);
+	mode_t mode = strtol(argv[1],NULL,8);//change string to mode type
 	res = chmod(argv[2],mode);
-
-	if(res == -1)
+	if(res == -1)//error
 	{
 		perror("Error");
 		exit(0);
