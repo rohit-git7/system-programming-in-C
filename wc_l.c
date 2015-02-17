60a3e0fc67eadcf9079c60e049a09c8039447df0
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-18 00:45:56 +0530

    Update wc_l.c

--- wc_l.c
+++ wc_l.c
@@ -1,17 +1,22 @@
+/*
+Implementing linux command 'wc -l' in C.
+To run:
+	./executable file_name
+*/
 #include<stdio.h>
-#include<fcntl.h>
-#include<unistd.h>
+#include<fcntl.h>//open() read()
+#include<unistd.h>//O_RDONLY
 int main(int argc, char *argv[])
 {
 	int n,count = 0;
 	char buff[2];
-	int fd = open(argv[1],O_RDONLY);
+	int fd = open(argv[1],O_RDONLY);//open file for reading
 	
-	while((n=read(fd,buff,1))>0)
+	while((n=read(fd,buff,1))>0)//reading file
 	{
 		buff[n] = '\0';
 		
-		if(buff[0] == '\n')
+		if(buff[0] == '\n')//counting newline characters
 		{
 			count++;
 		}
