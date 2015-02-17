55c9b22143a0c1ac5b919311177bdeb12b429e53
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-18 00:43:54 +0530

    Update wc_c.c

--- wc_c.c
+++ wc_c.c
@@ -1,15 +1,23 @@
+/*
+Implement linux command 'wc -c' in C.
+To run:
+	./executable file_name
+*/
 #include<stdio.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<sys/stat.h>
-#include<sys/types.h>
+#include<fcntl.h>//read() open()
+#include<unistd.h>//O_RDONLY
+#include<sys/stat.h>//fstat()
+
 int main(int argc, char *argv[])
 {
 	int n,count = 0;
-	char buff[2],last=' ';
+	char buff[2];
 	int fd = open(argv[1],O_RDONLY);
 	struct stat obj;
 	fstat(fd,&obj);
+	/*
+	We can get number of bytes from file stat structure or by counting each byte
+	*/
 	while((n=read(fd,buff,1))>0)
 	{
 		count++;
