26db158022fc0580063d11f68fb7f82e317f4f15
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-18 00:16:11 +0530

    Update wc_w.c

--- wc_w.c
+++ wc_w.c
@@ -9,15 +9,20 @@ To run:
 int main(int argc, char *argv[])
 {
 	int n,count = 0;
-	char buff[2];
+	char buff[2],last = ' ';
 	int fd=open(argv[1],O_RDONLY);//open file for reading
 	
 	while((n=read(fd,buff,1))>0)
 	{
 		buff[n] = '\0';
-		if(buff[0] == ' ' ||buff[0] == '\t' || buff[0] == '\n')//count words
+		if(buff[0] == ' ' || buff[0] == '\t' || buff[0] == '\n')
+		{
 			count++;
+			if(last == ' '|| last == '\t' || last == '\n')
+			count--;
+		}
+		last = buff[0];
 	}
-	printf("%d\n",count);
+	printf("%d %s\n",count,argv[1]);
 return 0;
 }
