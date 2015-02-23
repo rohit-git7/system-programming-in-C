f3341e6cc4c9ff54b3afe2acdd26f2e3978aa440
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-23 23:42:31 +0530

    Update cat.c

--- cat.c
+++ cat.c
@@ -1,8 +1,13 @@
+/*
+Implementing linux command 'cat' using C.
+To run:
+	./executable file_1 file_2 file_3 ... file_n
+*/
 #include<stdio.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<string.h>
-#include<stdlib.h>
+#include<fcntl.h>//open()
+#include<unistd.h>//read() write()
+#include<string.h>//strcpy() bzero()
+#include<stdlib.h>//exit()
 
 int main(int argc, char *argv[])
 {
@@ -11,20 +16,20 @@ int main(int argc, char *argv[])
 	char err[1024];
 	int n,i,fd;
 	
-	if(argc < 2)
+	if(argc < 2)//no file name specified
 	{
 		printf("No file name specified\n");
 		exit(1);
 	}
 	
-	for(i = 1;i < argc; i++)
+	for(i = 1;i < argc; i++)//for each file
 	{
 		strcpy(file,argv[i]);
 		strcpy(err,"cat: ");
 		strcat(err,file);
 
 		fd = open(file,O_RDONLY);
-		if(fd == -1)
+		if(fd == -1)//error in opening file
 		{
 			perror(err);
 			continue;
